333832aad7abdba84525419bdcc34950839e05ca
¨merve¨ <caylimerve@gmail.com>
2012-09-13 12:39:03 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jacquired::Application.config.secret_token = 'd9877691639c0544ebcc4f93ca0835cad31c62f6fb88253897478dc99f24dca5f254bdf734adb1bb6a6a57a06a03287c063f52fcf8eae1f054831ac026bff386'
